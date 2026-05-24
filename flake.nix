6916c4b1fc7e14f17a2bd1bc5f75ba308c98c0f5
Givi <gvtsvariani@gmail.com>
2026-05-24 18:03:16 -0400

    flake shenanigans

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix Flake for a Qt C++ Frontend with Auth0 integration";
+  description = "Qt-auth0 test flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -36,12 +36,10 @@
           ];
 
           shellHook = ''
-            echo "⚡ Qt C++ Dev Shell Enabled ⚡"
+            echo "Qt C++ Dev Shell Enabled"
 
-            # Export standard build environments
             export CMAKE_GENERATOR="Ninja"
 
-            # Crucial fixes for Qt applications running out of build trees on Linux
             export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}/platforms"
             export QML2_IMPORT_PATH="${pkgs.qt6.qtdeclarative}/${pkgs.qt6.qtbase.qtQmlPrefix}"
 
