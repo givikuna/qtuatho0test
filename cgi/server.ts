4b4c4a23f83c3d1774b2ba4327d912f80404a1a8
Givi <gvtsvariani@gmail.com>
2026-06-01 11:55:05 -0400

    breakpoint

--- cgi/server.ts
+++ cgi/server.ts
@@ -1,10 +1,26 @@
 import * as express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+import { auth, ConfigParams } from "express-openid-connect";
 
 import { router } from "../routes/router";
 
 const PORT: number = 3000;
 
 const app: express.Application = express.default();
+
+const config: ConfigParams = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env["SECRET"]!,
+    baseURL: process.env["BASE_URL"]!,
+    clientID: process.env["CLIENT_ID"]!,
+    issuerBaseURL: process.env["ISSUER_BASE_URL"]!,
+};
+
+app.use(auth(config));
 
 app.use("/", router);
 
--- routes/router.ts
+++ routes/router.ts
@@ -1,8 +1,23 @@
 import * as express from "express";
-import * as path from "path";
 
 export const router: express.Router = express.Router();
 
-router.get("/", (_req: express.Request, res: express.Response): void => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/", (req: express.Request, res: express.Response): void => {
+    if (!req.oidc.isAuthenticated()) {
+        res.type("html").send(/* HTML */ `
+            <h1>auth0:</h1>
+            <p>not logged in.</p>
+            <a href="/signup">Sign up</a><br />
+            <a href="/login">Log in</a>
+        `);
+        return;
+    }
+
+    const username: string = req.oidc.user?.["name"];
+    res.type("html").send(/* HTML */ `
+        <h1>profiles:</h1>
+        <p>logged in as ${username}</p>
+        <pre>${JSON.stringify(req.oidc.user, null, 2)}</pre>
+        <a href="/logout">Log out</a>
+    `);
 });
