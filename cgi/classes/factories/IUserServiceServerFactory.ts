ccef738d9720328453330ef88a3f6392290eaf18
Givi <gvtsvariani@gmail.com>
2026-06-08 17:14:41 -0400

    moved UserServices class into just a factory for IUserServiceServer class because UserServices was unable to properly take the right shape for the implementation for type defs :(

---
+++ cgi/classes/factories/IUserServiceServerFactory.ts
@@ -0,0 +1,71 @@
+import * as grpc from "@grpc/grpc-js";
+import * as jwksClient from "jwks-rsa";
+import * as jwt from "jsonwebtoken";
+
+import { IUserServiceServer } from "../../../src/proto/proto/user_grpc_pb";
+import {
+    GetProfileRequest,
+    GetProfileResponse,
+} from "../../../src/proto/proto/user_pb";
+
+import { verifyToken } from "../../helpers/verifyToken";
+
+export class IUserServiceServerFactory {
+    public static createUserService(
+        client: jwksClient.JwksClient,
+        CLIENT_ID: string,
+        ISSUER_BASE_URL: string,
+    ): IUserServiceServer {
+        return {
+            getProfile: async (
+                call: grpc.ServerUnaryCall<
+                    GetProfileRequest,
+                    GetProfileResponse
+                >,
+                callback: grpc.sendUnaryData<GetProfileResponse>,
+            ): Promise<void> => {
+                try {
+                    const authHeader: grpc.MetadataValue[] =
+                        call.metadata.get("authorization");
+
+                    if (!authHeader || authHeader.length === 0) {
+                        return callback({
+                            code: grpc.status.UNAUTHENTICATED,
+                            message: "missing authorization metadata",
+                        });
+                    }
+
+                    const token: string = authHeader[0]
+                        .toString()
+                        .replace("Bearer ", "");
+
+                    const decoded: jwt.JwtPayload = await verifyToken(
+                        token,
+                        client,
+                        CLIENT_ID,
+                        ISSUER_BASE_URL,
+                    );
+
+                    const response: GetProfileResponse =
+                        new GetProfileResponse();
+
+                    response.setName(decoded["name"] || "unknown");
+                    response.setEmail(decoded["email"] || "unknown");
+                    response.setPicture(decoded["picture"] || "unknown");
+
+                    callback(null, response);
+                } catch (e: unknown) {
+                    console.error(e);
+
+                    callback(
+                        {
+                            code: grpc.status.UNAUTHENTICATED,
+                            message: "invalid or expired token",
+                        },
+                        null,
+                    );
+                }
+            },
+        };
+    }
+}
--- cgi/server.ts
+++ cgi/server.ts
@@ -1,11 +1,40 @@
 import * as dotenv from "dotenv";
 import * as jwksClient from "jwks-rsa";
+import * as grpc from "@grpc/grpc-js";
 
-// enables env variables
+import { UserServiceService } from "../src/proto/proto/user_grpc_pb";
+
+import { IUserServiceServerFactory } from "./classes/factories/IUserServiceServerFactory";
+
 dotenv.config();
+
+const PORT = process.env["PORT"] || 3000;
 
 // for fetching auth's public keys
 // will verify incoming tokens
 const client: jwksClient.JwksClient = jwksClient.default({
-    jwksUri: `{process.env["ISSUER_BASE_URL"]}/.well-known/jwks.json`,
+    jwksUri: `${process.env["ISSUER_BASE_URL"]}/.well-known/jwks.json`,
 });
+
+const server: grpc.Server = new grpc.Server();
+
+server.addService(
+    UserServiceService,
+    IUserServiceServerFactory.createUserService(
+        client,
+        process.env["CLIENT_ID"]!,
+        process.env["ISSUER_BASE_URL"]!,
+    ),
+);
+
+server.bindAsync(
+    `0.0.0.0:${PORT}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err, port) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`running on ${port}`);
+    },
+);
