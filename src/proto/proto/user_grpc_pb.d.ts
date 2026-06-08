eafd5449f854ae6c828542bbbbf9587c84ac27a5
Givi <gvtsvariani@gmail.com>
2026-06-08 17:16:59 -0400

    proto format & proto linguist

--- src/proto/proto/user_grpc_pb.d.ts
+++ src/proto/proto/user_grpc_pb.d.ts
@@ -11,7 +11,10 @@ interface IUserServiceService extends grpc.ServiceDefinition<grpc.UntypedService
     getProfile: IUserServiceService_IGetProfile;
 }
 
-interface IUserServiceService_IGetProfile extends grpc.MethodDefinition<proto_user_pb.GetProfileRequest, proto_user_pb.GetProfileResponse> {
+interface IUserServiceService_IGetProfile extends grpc.MethodDefinition<
+    proto_user_pb.GetProfileRequest,
+    proto_user_pb.GetProfileResponse
+> {
     path: "/user.UserService/GetProfile";
     requestStream: false;
     responseStream: false;
@@ -24,18 +27,70 @@ interface IUserServiceService_IGetProfile extends grpc.MethodDefinition<proto_us
 export const UserServiceService: IUserServiceService;
 
 export interface IUserServiceServer extends grpc.UntypedServiceImplementation {
-    getProfile: grpc.handleUnaryCall<proto_user_pb.GetProfileRequest, proto_user_pb.GetProfileResponse>;
+    getProfile: grpc.handleUnaryCall<
+        proto_user_pb.GetProfileRequest,
+        proto_user_pb.GetProfileResponse
+    >;
 }
 
 export interface IUserServiceClient {
-    getProfile(request: proto_user_pb.GetProfileRequest, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
-    getProfile(request: proto_user_pb.GetProfileRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
-    getProfile(request: proto_user_pb.GetProfileRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
+    getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
+    getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        metadata: grpc.Metadata,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
+    getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        metadata: grpc.Metadata,
+        options: Partial<grpc.CallOptions>,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
 }
 
-export class UserServiceClient extends grpc.Client implements IUserServiceClient {
-    constructor(address: string, credentials: grpc.ChannelCredentials, options?: Partial<grpc.ClientOptions>);
-    public getProfile(request: proto_user_pb.GetProfileRequest, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
-    public getProfile(request: proto_user_pb.GetProfileRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
-    public getProfile(request: proto_user_pb.GetProfileRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_user_pb.GetProfileResponse) => void): grpc.ClientUnaryCall;
+export class UserServiceClient
+    extends grpc.Client
+    implements IUserServiceClient
+{
+    constructor(
+        address: string,
+        credentials: grpc.ChannelCredentials,
+        options?: Partial<grpc.ClientOptions>,
+    );
+    public getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
+    public getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        metadata: grpc.Metadata,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
+    public getProfile(
+        request: proto_user_pb.GetProfileRequest,
+        metadata: grpc.Metadata,
+        options: Partial<grpc.CallOptions>,
+        callback: (
+            error: grpc.ServiceError | null,
+            response: proto_user_pb.GetProfileResponse,
+        ) => void,
+    ): grpc.ClientUnaryCall;
 }
--- src/proto/proto/user_pb.d.ts
+++ src/proto/proto/user_pb.d.ts
@@ -6,24 +6,33 @@
 
 import * as jspb from "google-protobuf";
 
-export class GetProfileRequest extends jspb.Message { 
-
+export class GetProfileRequest extends jspb.Message {
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): GetProfileRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: GetProfileRequest): GetProfileRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: GetProfileRequest, writer: jspb.BinaryWriter): void;
+    static toObject(
+        includeInstance: boolean,
+        msg: GetProfileRequest,
+    ): GetProfileRequest.AsObject;
+    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+    static extensionsBinary: {
+        [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+    };
+    static serializeBinaryToWriter(
+        message: GetProfileRequest,
+        writer: jspb.BinaryWriter,
+    ): void;
     static deserializeBinary(bytes: Uint8Array): GetProfileRequest;
-    static deserializeBinaryFromReader(message: GetProfileRequest, reader: jspb.BinaryReader): GetProfileRequest;
+    static deserializeBinaryFromReader(
+        message: GetProfileRequest,
+        reader: jspb.BinaryReader,
+    ): GetProfileRequest;
 }
 
 export namespace GetProfileRequest {
-    export type AsObject = {
-    }
+    export type AsObject = {};
 }
 
-export class GetProfileResponse extends jspb.Message { 
+export class GetProfileResponse extends jspb.Message {
     getName(): string;
     setName(value: string): GetProfileResponse;
     getEmail(): string;
@@ -33,18 +42,29 @@ export class GetProfileResponse extends jspb.Message {
 
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): GetProfileResponse.AsObject;
-    static toObject(includeInstance: boolean, msg: GetProfileResponse): GetProfileResponse.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: GetProfileResponse, writer: jspb.BinaryWriter): void;
+    static toObject(
+        includeInstance: boolean,
+        msg: GetProfileResponse,
+    ): GetProfileResponse.AsObject;
+    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+    static extensionsBinary: {
+        [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+    };
+    static serializeBinaryToWriter(
+        message: GetProfileResponse,
+        writer: jspb.BinaryWriter,
+    ): void;
     static deserializeBinary(bytes: Uint8Array): GetProfileResponse;
-    static deserializeBinaryFromReader(message: GetProfileResponse, reader: jspb.BinaryReader): GetProfileResponse;
+    static deserializeBinaryFromReader(
+        message: GetProfileResponse,
+        reader: jspb.BinaryReader,
+    ): GetProfileResponse;
 }
 
 export namespace GetProfileResponse {
     export type AsObject = {
-        name: string,
-        email: string,
-        picture: string,
-    }
+        name: string;
+        email: string;
+        picture: string;
+    };
 }
